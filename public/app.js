dfa9083a87a777263b011134801a2978c8490e95
J noe Bustos <jb7noah@gmail.com>
2019-03-20 21:55:59 -0400

    header js scroll functionality finished

--- public/app.js
+++ public/app.js
@@ -1,10 +1,25 @@
 window.onscroll = function() {
   var header = document.querySelector('#header');
-  if (this.scrollY <= 10){
-    header.className = ''
-  }  else {
+  // if (this.scrollY <= 10){
+  //   header.className = ''
+  // }  else {
+  //   header.className = 'scroll'
+  // }
+
+
+  if (window.pageYOffset > window.innerHeight){
+    console.log("what.........")
     header.className = 'scroll'
+  }  
+  else {
+    header.className = ''
   }
+  // if (window.pageYOffset < window.innerHeight) {
+  //   console.log("what no way.........")
+  //   header.classList.toggle = 'no-scroll'
+  // } 
 }
 
 console.log("js file working")
+
+
