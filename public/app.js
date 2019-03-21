4e61009c2b517b978d1172f83a77b21ade0fe4f1
J noe Bustos <jb7noah@gmail.com>
2019-03-20 23:25:09 -0400

    app.js code clean up

--- public/app.js
+++ public/app.js
@@ -1,12 +1,6 @@
 window.onscroll = function() {
   var header = document.querySelector('#header');
-  // if (this.scrollY <= 10){
-  //   header.className = ''
-  // }  else {
-  //   header.className = 'scroll'
-  // }
-
-
+  
   if (window.pageYOffset > window.innerHeight){
     console.log("what.........")
     header.className = 'scroll'
@@ -14,12 +8,4 @@ window.onscroll = function() {
   else {
     header.className = ''
   }
-  // if (window.pageYOffset < window.innerHeight) {
-  //   console.log("what no way.........")
-  //   header.classList.toggle = 'no-scroll'
-  // } 
 }
-
-console.log("js file working")
-
-
