d107e0d50b74d862cb247b04ba9da8c55546bd44
J noe Bustos <jb7noah@gmail.com>
2019-03-25 22:18:41 -0400

    header mobile menu icon refactor

--- public/app.js
+++ public/app.js
@@ -1,7 +1,8 @@
 const header = document.querySelector('#header');
 const navLink = document.querySelector("header nav a")
-const hamburgerIcon = document.querySelector(".fa-bars");
-const outIcon = document.querySelector(".fa-times");
+const hamburgerIcon = document.querySelector(".hamburger-icon");
+const outIcon = document.querySelector(" .out-icon");
+const mobileMenuIcons = document.querySelector(" .mobile-menu-icons");
 
 // window.onscroll = function() {
   
@@ -20,9 +21,24 @@ window.onscroll = () => {
 console.log(hamburgerIcon);
 console.log(outIcon);
 
-hamburgerIcon.addEventListener("click", function(){
-  hamburgerIcon.classList.toggle("active");
-  // outIcon.classList.toggle("active");
-  console.log(hamburgerIcon);
-  console.log(this);
+mobileMenuIcons.addEventListener("click", function(){
+  hamburgerIcon.classList.toggle("unactive");
+  outIcon.classList.toggle("unactive");
 })
+
+
+
+
+// hamburgerIcon.addEventListener("click", function(){
+//   this.classList.toggle("active");
+//   outIcon.classList.toggle("active");
+//   console.log(hamburgerIcon);
+//   console.log(this);
+// })
+
+// outIcon.addEventListener("click", function(){
+//   this.classList.toggle("active");
+//   hamburgerIcon.classList.toggle("active");
+//   console.log(outIcon);
+//   console.log(this);
+// })
