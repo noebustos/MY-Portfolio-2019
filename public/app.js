0d5b85f8c243d95112b7a921febb112910460bdf
J noe Bustos <jb7noah@gmail.com>
2019-03-26 20:46:56 -0400

    header mobile menu functionality

--- public/app.js
+++ public/app.js
@@ -2,7 +2,9 @@ const header = document.querySelector('#header');
 const navLink = document.querySelector("header nav a")
 const hamburgerIcon = document.querySelector(".hamburger-icon");
 const outIcon = document.querySelector(" .out-icon");
-const mobileMenuIcons = document.querySelector(" .mobile-menu-icons");
+const mobileMenuIcons = document.querySelector(".mobile-menu-icons");
+const headerContainer = document.querySelector("header .container");
+console.log(headerContainer);
 
 // window.onscroll = function() {
   
@@ -24,6 +26,8 @@ console.log(outIcon);
 mobileMenuIcons.addEventListener("click", function(){
   hamburgerIcon.classList.toggle("unactive");
   outIcon.classList.toggle("unactive");
+  headerContainer.classList.toggle("unactive");
+  header.classList.toggle("active");
 })
 
 
@@ -42,3 +46,22 @@ mobileMenuIcons.addEventListener("click", function(){
 //   console.log(outIcon);
 //   console.log(this);
 // })
+
+
+
+
+
+
+
+
+
+var john = {
+  // billValues: [124, 48, 268, 180, 42],
+  // //add a method to calc the tip.
+  // //this should include a loop to iterate over all the paid bills and do the tip calcs
+  // tipBill: function(){
+  //   for(var i = 0; i < this.billValues.length -1 ; i++){
+             
+  //   }
+  // },
+}
