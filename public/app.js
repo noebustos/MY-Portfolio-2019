919515888df96eb77f69930242364d900522250f
J noe Bustos <jb7noah@gmail.com>
2019-03-26 21:56:42 -0400

    mobile menu work

--- public/app.js
+++ public/app.js
@@ -4,6 +4,7 @@ const hamburgerIcon = document.querySelector(".hamburger-icon");
 const outIcon = document.querySelector(" .out-icon");
 const mobileMenuIcons = document.querySelector(".mobile-menu-icons");
 const headerContainer = document.querySelector("header .container");
+
 console.log(headerContainer);
 
 // window.onscroll = function() {
@@ -16,8 +17,29 @@ console.log(headerContainer);
 //   }
 // }
 
+console.log(mobileMenuIcons.style)
+
 window.onscroll = () => {
-  if (this.scrollY <= 350) header.className = ''; else header.className = 'scroll';
+  if (this.scrollY <= 350) {
+    header.className = '';
+    console.log("scroll is active and   mobile menu is on none")
+  }  
+
+  // else if (this.scrollY <= 350 && mobileMenuIcons.style.display == "block"){
+  //   header.classList.add("active");
+  //   header.classList.remove("scroll");
+  //   console.log("scroll is active and   mobile menu is on display")
+  // }
+
+  else{
+    header.className = 'scroll';
+  }
+
+
+  // if (headerContainer.classname == 'unactive'){
+  //   header.classList.toggle("active");
+  //   header.classList.toggle("scroll");
+  // }
 };
 
 console.log(hamburgerIcon);
