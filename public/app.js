c22561c9202b1d1f765faba6a88fe76b095c794a
J Noe Bustos <jb7noah@gmail.com>
2019-06-07 22:59:57 -0400

    header edits

--- public/app.js
+++ public/app.js
@@ -5,7 +5,6 @@ const outIcon = document.querySelector(" .out-icon");
 const mobileMenuIcons = document.querySelector(".mobile-menu-icons");
 const headerContainer = document.querySelector("header .container");
 
-console.log(headerContainer);
 
 // window.onscroll = function() {
   
@@ -21,30 +20,38 @@ console.log(headerContainer);
 
 //we want the hamburger menu to only be displayed when the user scrolls down the page. righ now its showing the white bg,with the icon, during scroll action.
 //also it would be good to look at other sites to see how they implement the mobile icon on their site. right now im thinking to have it on the top right at all times with a white padding behind it or maybe just some  boxshadow behind it. something to show separation. bc with the icon alone it blends into the site's content too much. and we want this icon to standout bc its a main navigation element.
+ 
+//we want to add a blur bg to the mobile menue icon so that it hass a place to sit on the page.
 
-console.log(mobileMenuIcons.style)
+
+console.log('this is my window width size: ' + window.innerWidth);
 
 window.onscroll = () => {
-  if (this.scrollY <= 350) {
-    header.className = '';
-    console.log("scroll is active and   mobile menu is on none")
-  }  
+  //we want to add an if statement to check when ever we scroll up/down, for our menu to change on mobile view.
+  if(window.innerWidth <= 479 ){
+    console.log("look mama i made it")
 
-  // else if (this.scrollY <= 350 && mobileMenuIcons.style.display == "block"){
-  //   header.classList.add("active");
-  //   header.classList.remove("scroll");
-  //   console.log("scroll is active and   mobile menu is on display")
-  // }
+    if (this.scrollY <= 200) {
+      header.className = '';
+      console.log("scroll is active and   mobile menu is on none")
+    }  
+  
+    // else{
+    //   header.className = 'scroll';
+    // }
 
-  else{
-    header.className = 'scroll';
+
+
+  } else{
+    if (this.scrollY <= 350) {
+      header.className = '';
+      console.log("scroll is active and   mobile menu is on none")
+    }  
+  
+    else{
+      header.className = 'scroll';
+    }
   }
-
-
-  // if (headerContainer.classname == 'unactive'){
-  //   header.classList.toggle("active");
-  //   header.classList.toggle("scroll");
-  // }
 };
 
 console.log(hamburgerIcon);
