a4d24032b79106e487769a988d6a41fefaa85731
J Noe Bustos <jb7noah@gmail.com>
2019-06-11 22:40:55 -0400

    mobile header upload

--- public/app.js
+++ public/app.js
@@ -16,34 +16,33 @@ const headerContainer = document.querySelector("header .container");
 //   }
 // }
 
+var winSize = window.scrollY
+console.log("scroll size: " + winSize);
+//about me sec is about 731
+//skills section scroll size: 1924
+//projects section scroll size:  2573
+//contact section scroll size: 4799 a little more
 
-
-//we want the hamburger menu to only be displayed when the user scrolls down the page. righ now its showing the white bg,with the icon, during scroll action.
-//also it would be good to look at other sites to see how they implement the mobile icon on their site. right now im thinking to have it on the top right at all times with a white padding behind it or maybe just some  boxshadow behind it. something to show separation. bc with the icon alone it blends into the site's content too much. and we want this icon to standout bc its a main navigation element.
- 
-//we want to add a blur bg to the mobile menue icon so that it hass a place to sit on the page.
 
 
 console.log('this is my window width size: ' + window.innerWidth);
 
 //scroll events for header
 window.onscroll = () => {
-  //we want to add an if statement to check when ever we scroll up/down, for our menu to change on mobile view.
+  //if screen is on mobile view.
   if(window.innerWidth <= 479 ){
-    console.log("look mama i made it")
+    console.log("MAX window width screen is <= 479px")
 
-    if (this.scrollY <= 200) {
-      // header.className = '';
-      console.log("scroll is active and   mobile menu is on none")
-    }  
-  
-    else{
-      header.className = 'scroll';
+    if (this.scrollY <=175) {
+      header.classList.remove('scroll');
+    } else{
+      header.classList.add('scroll');
+      console.log("scroll is active")
+      console.log(header.className.split(' '));
     }
-//scroll is misbehaving. scroll and active classess change when scrolling. so we can only get one at a time.
-
-
-  } else{
+  } 
+  //if screen is NOT on mobile view.
+  else{
     if (this.scrollY <= 350) {
       header.className = '';
       console.log("scroll is active and   mobile menu is on none")
@@ -55,28 +54,20 @@ window.onscroll = () => {
   }
 };
 
-console.log(hamburgerIcon);
-console.log(outIcon);
 
-
-//click event for mobible menu btns
+// click event for mobible menu btns
 mobileMenuIcons.addEventListener("click", function(){
-
+  console.log("clicked mobile menu");
+  console.log(hamburgerIcon);
+  console.log(outIcon);
+  if(header.classList.contains("header-close")){
+    console.log("hamburger icon clicked and display header is now opened")
+    header.classList.remove("header-close");
+    header.classList.add("header-open");
+  } else if(header.classList.contains("header-open")){
+    console.log("out icon clicked and display header is now closed")
+    header.classList.remove("header-open");
+    header.classList.add("header-close");
+  }
 })
 
-
-
-
-// hamburgerIcon.addEventListener("click", function(){
-//   this.classList.toggle("active");
-//   outIcon.classList.toggle("active");
-//   console.log(hamburgerIcon);
-//   console.log(this);
-// })
-
-// outIcon.addEventListener("click", function(){
-//   this.classList.toggle("active");
-//   hamburgerIcon.classList.toggle("active");
-//   console.log(outIcon);
-//   console.log(this);
-// })
